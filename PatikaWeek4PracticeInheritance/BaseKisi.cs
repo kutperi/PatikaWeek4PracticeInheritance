ebda56320a99528252494df43cc435e11de13bd8
Bilal <bilalbilin12@gmail.com>
2024-08-07 16:47:26 +0300

    Add project files.

---
+++ PatikaWeek4PracticeInheritance/BaseKisi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace PatikaWeek4PracticeInheritance
+{
+    public class BaseKisi // Creating a new base class
+    {
+        public string Ad { get; set; } // Setting 2 common properties
+
+        public string Soyad { get; set; }
+
+        public void DisplayFullName() // Method to call full name
+        {
+            Console.WriteLine($"Tüm ad: {Ad} {Soyad}");
+        }
+
+    }
+
+    public class Ogrenci : BaseKisi // Creating a new class that inherits from a base class
+    {
+        public int StudentNumber { get; set; } // Setting a new property for this class
+
+        public void DisplayFullNameAndNumber() // Method that display full name and student number
+        {
+            DisplayFullName();
+            Console.WriteLine($"Öğrenci Numarası: {StudentNumber}");
+        }
+
+    }
+
+    public class Ogretmen : BaseKisi // Creating a new class that inherits from a base class
+    {
+        public decimal Salary { get; set; } // Setting a new property for this class
+
+        public void DisplayFullNameAndSalary() // Method that display full name and salary
+        {
+            DisplayFullName();
+            Console.WriteLine($"Öğretmenin Maaşı: {Salary} TL");
+        }
+    }
+}
---
+++ PatikaWeek4PracticeInheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using PatikaWeek4PracticeInheritance;
+
+Ogrenci student = new Ogrenci(); // Creating an instance for Ogrenci
+
+// Settting properties of Ogrenci
+student.Ad = "Bilal";
+student.Soyad = "Bilin";
+student.StudentNumber = 2174112;
+
+student.DisplayFullNameAndNumber(); // Calling the method that display full name and student number
+
+Ogretmen teacher = new Ogretmen(); // Creating an instance for Ogretmen
+
+// Setting properties of Ogretmen
+teacher.Ad = "Disa";
+teacher.Soyad = "Dilan";
+teacher.Salary = 17002;
+
+teacher.DisplayFullNameAndSalary(); // Calling the method that display full name and salary
